dd808241e4ef6a69f668b6409715ec09c78efe86
MarceloFirmato <116672694+MarceloFirmato@users.noreply.github.com>
2023-01-23 10:45:13 -0300

    runbuild

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Janela", "Farol"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Goiaba", "Pitaya", "Manga"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Panturrilha"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "RAM", "Memória"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Python", "Ruby"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  };
